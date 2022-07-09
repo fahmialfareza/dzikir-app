9cc0d8a2bb8a2d62a373eda0059b39e804367978
Ahmad Aji Santoso <49109789+santoadji21@users.noreply.github.com>
2022-07-09 16:50:12 +0700

    add modal

--- screens/dzikir/DzikirCounter.tsx
+++ screens/dzikir/DzikirCounter.tsx
@@ -1,32 +1,35 @@
-import React, { useState, useEffect } from 'react';
+import { FontAwesome } from "@expo/vector-icons";
+import { RouteProp } from "@react-navigation/native";
+import { Audio } from "expo-av";
+import React, { useEffect, useState } from "react";
 import {
-  View,
-  useColorScheme,
-  Text,
-  StyleSheet,
   Alert,
   Dimensions,
-  TextInput,
-} from 'react-native';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { RouteProp } from '@react-navigation/native';
-import { connect } from 'react-redux';
-import { ThunkDispatch } from 'redux-thunk';
-import { Audio } from 'expo-av';
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from "react-native";
+import Dialog from "react-native-dialog";
+import {
+  SafeAreaView,
+  useSafeAreaInsets,
+} from "react-native-safe-area-context";
+import { connect } from "react-redux";
+import { ThunkDispatch } from "redux-thunk";
 
-import DzikirTarget from '../../models/dzikirTarget';
+import DzikirTarget from "../../models/dzikirTarget";
 
-import screenMode from '../../constants/screenMode';
-import { DzikirCounterBackgrondImage } from '../../constants/assets';
+import { DzikirCounterBackgrondImage } from "../../constants/assets";
+import screenMode from "../../constants/screenMode";
 
-import TasbihCounter from '../../components/TasbihCounter';
+import TasbihCounter from "../../components/TasbihCounter";
 
-import { RootState } from '../../redux';
-import { updateDzikirTarget } from '../../redux/actions/dzikirTarget';
+import { RootState } from "../../redux";
+import { updateDzikirTarget } from "../../redux/actions/dzikirTarget";
 
 interface DzikirCounterProps {
-  route: RouteProp<{ params: { item: DzikirTarget } }, 'params'>;
+  route: RouteProp<{ params: { item: DzikirTarget } }, "params">;
   updateDzikirTarget: (
     id: number,
     title: string,
@@ -56,17 +59,18 @@ function DzikirCounter({
 }: DzikirCounterProps) {
   const colorScheme = useColorScheme();
   const insets = useSafeAreaInsets();
-
-  const [count, setCount] = useState('0000');
-  const [addColor, setAddColor] = useState('#FCDDEC');
-  const [resetColor, setResetColor] = useState('#FCDDEC');
+  const [count, setCount] = useState("0000");
+  const [addColor, setAddColor] = useState("#FCDDEC");
+  const [resetColor, setResetColor] = useState("#FCDDEC");
+  const [visible, setVisible] = useState(false);
+  const [visibleResetDialog, setvisibleResetDialog] = useState(false);
 
   // State value of dzikir
   const [oldTarget, setOldTarget] = useState(String(params.item.target));
   const [target, setTarget] = useState(String(params.item.target));
 
   const themeContainerStyle =
-    colorScheme === 'light'
+    colorScheme === "light"
       ? screenMode.lightContainer
       : screenMode.darkContainer;
 
@@ -75,11 +79,37 @@ function DzikirCounter({
       params.item.id,
       params.item.title,
       parseInt(target),
-      params.item.arabic || '',
-      params.item.background || '',
-      params.item.color || '',
+      params.item.arabic || "",
+      params.item.background || "",
+      params.item.color || "",
       parseInt(count)
     );
+  };
+
+  const showDialog = () => {
+    setVisible(true);
+  };
+
+  const handleCancel = () => {
+    setVisible(false);
+  };
+
+  const handleSaveTarget = () => {
+    setTarget(target);
+    submitTargetHandler();
+    setVisible(false);
+  };
+
+  const resetDialog = () => {
+    if (count === "0000") {
+      setvisibleResetDialog(false);
+    } else {
+      setvisibleResetDialog(true);
+    }
+  };
+
+  const handleCancelResetDialog = () => {
+    setvisibleResetDialog(false);
   };
 
   const counterFormatter = (countNumber: number) => {
@@ -104,7 +134,7 @@ function DzikirCounter({
     let result = counterFormatter(countNumber);
 
     setTimeout(() => {
-      setAddColor('#FCDDEC');
+      setAddColor("#FCDDEC");
     }, 100);
 
     setCount(result);
@@ -115,20 +145,21 @@ function DzikirCounter({
     setResetColor(themeContainerStyle.backgroundColor);
 
     setTimeout(() => {
-      setResetColor('#FCDDEC');
+      setResetColor("#FCDDEC");
     }, 100);
 
-    setCount('0000');
+    setCount("0000");
+    setvisibleResetDialog(false);
   };
 
   const targetChangeTextHandler = (text: string) => {
-    if (text === '') {
-      setTarget('0');
+    if (text === "") {
+      setTarget("0");
       return;
     }
 
-    if (target === '0') {
-      setTarget(text.split('0').join(''));
+    if (target === "0") {
+      setTarget(text.split("0").join(""));
       return;
     }
 
@@ -137,7 +168,7 @@ function DzikirCounter({
 
   const submitTargetHandler = async () => {
     try {
-      if (target !== '0') {
+      if (target !== "0") {
         if (target === oldTarget) {
           return;
         }
@@ -146,8 +177,8 @@ function DzikirCounter({
 
         setOldTarget(target);
 
-        Alert.alert('Berhasil Mengganti Target', `Target menjadi ${target}`, [
-          { text: 'OK', onPress: () => console.log('Target OK') },
+        Alert.alert("Berhasil Mengganti Target", `Target menjadi ${target}`, [
+          { text: "OK", onPress: () => console.log("Target OK") },
         ]);
       }
     } catch (error) {
@@ -160,10 +191,10 @@ function DzikirCounter({
   }, [params.item.counter]);
 
   useEffect(() => {
-    if (target !== '0') {
+    if (target !== "0") {
       if (parseInt(count) === parseInt(target)) {
-        Alert.alert('Alhamdullah!', 'Kamu telah mencapai target dzikir', [
-          { text: 'OK', onPress: async () => console.log('Target OK') },
+        Alert.alert("Alhamdullah!", "Kamu telah mencapai target dzikir", [
+          { text: "OK", onPress: async () => console.log("Target OK") },
         ]);
         successAudio();
       }
@@ -177,7 +208,7 @@ function DzikirCounter({
   const successAudio = async () => {
     const sound = new Audio.Sound();
     try {
-      await sound.loadAsync(require('../../assets/sounds/success.wav'));
+      await sound.loadAsync(require("../../assets/sounds/success.wav"));
       await sound.playAsync();
       // Your sound is playing!
 
@@ -197,9 +228,9 @@ function DzikirCounter({
           ...styles.backgroundImage,
           top: -(1.5 * insets.top + 1.5 * insets.bottom),
         }}
-        width={Dimensions.get('window').width}
+        width={Dimensions.get("window").width}
         height={
-          Dimensions.get('window').height +
+          Dimensions.get("window").height +
           1.5 * insets.top +
           1.5 * insets.bottom
         }
@@ -210,16 +241,36 @@ function DzikirCounter({
           <Text style={styles.dzikirText}>{params.item.title}</Text>
           <View style={styles.targetView}>
             <View>
-              <Text style={styles.inputText}>Target </Text>
+              <Text style={styles.inputText}>Target :</Text>
             </View>
             <View>
-              <TextInput
-                style={styles.targetInput}
-                onChangeText={targetChangeTextHandler}
-                onEndEditing={submitTargetHandler}
-                value={String(target)}
-                keyboardType="number-pad"
-                returnKeyType={'done'}
+              <Dialog.Container visible={visible}>
+                <Dialog.Title>Masukan Target</Dialog.Title>
+                <Dialog.Input
+                  onChangeText={targetChangeTextHandler}
+                  keyboardType="number-pad"
+                  returnKeyType={"done"}
+                  style={styles.dialogInput}
+                >
+                  {target}
+                </Dialog.Input>
+                <Dialog.Button
+                  style={styles.saveTarget}
+                  label="Simpan"
+                  onPress={handleSaveTarget}
+                />
+                <Dialog.Button label="Batal" onPress={handleCancel} />
+              </Dialog.Container>
+            </View>
+            <View>
+              <Text style={styles.targetText}>{String(target)}</Text>
+            </View>
+            <View style={styles.editButton}>
+              <FontAwesome
+                name="pencil-square"
+                size={32}
+                color="white"
+                onPress={showDialog}
               />
             </View>
           </View>
@@ -229,10 +280,24 @@ function DzikirCounter({
           <TasbihCounter
             count={count}
             addColor={addColor}
-            resetColor={resetColor}
+            resetColor={parseInt(count) > 0 ? "#FABF39" : resetColor}
             addCounterHandler={addCounterHandler}
-            resetCounterHandler={resetCounterHandler}
+            resetCounterHandler={resetDialog}
           />
+        </View>
+        <View>
+          <Dialog.Container visible={visibleResetDialog}>
+            <Dialog.Title>Reset Hitungan</Dialog.Title>
+            <Dialog.Description>
+              Apakah anda ingin mereset hitungan yang sudah berjalan?
+            </Dialog.Description>
+            <Dialog.Button
+              label="Reset"
+              style={styles.resetCounter}
+              onPress={resetCounterHandler}
+            />
+            <Dialog.Button label="Batal" onPress={handleCancelResetDialog} />
+          </Dialog.Container>
         </View>
       </SafeAreaView>
     </>
@@ -240,72 +305,111 @@ function DzikirCounter({
 }
 
 export const screenOptions = {
-  headerTitle: 'Dzikir Counter',
+  headerTitle: "Dzikir Counter",
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  editButton: {
+    marginTop: 3,
+    marginLeft: 10,
   },
   textView: {
     flex: 2,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     padding: 10,
     marginTop: 40,
   },
   arabicText: {
-    fontFamily: 'dubai-regular',
-    justifyContent: 'center',
-    alignContent: 'center',
+    fontFamily: "dubai-regular",
+    justifyContent: "center",
+    alignContent: "center",
     fontSize: 64,
-    color: '#FFFFFF',
+    color: "#FFFFFF",
   },
   dzikirText: {
-    fontFamily: 'dubai-regular',
-    justifyContent: 'center',
-    alignContent: 'center',
+    fontFamily: "dubai-regular",
+    justifyContent: "center",
+    alignContent: "center",
     fontSize: 32,
-    color: '#FFFFFF',
+    color: "#FFFFFF",
+  },
+  targetText: {
+    marginTop: 2,
+    fontSize: 24,
+    fontFamily: "dubai-regular",
+    fontWeight: "bold",
+    justifyContent: "center",
+    alignContent: "center",
+    marginRight: 10,
+    color: "#FFFFFF",
+  },
+  saveTarget: {
+    fontSize: 20,
+    color: "#FFFFFF",
+    backgroundColor: "#00BFFF",
+    width: "100%",
+    borderRadius: 10,
+  },
+  resetCounter: {
+    marginTop: 20,
+    marginBottom: 10,
+    fontSize: 16,
+    color: "#FFFFFF",
+    backgroundColor: "#FF4444",
+    width: "100%",
+    borderRadius: 10,
+  },
+  dialogInput: {
+    marginTop: 20,
+    fontSize: 20,
+    fontFamily: "dubai-regular",
+    fontWeight: "bold",
+    justifyContent: "center",
+    alignContent: "center",
+    marginRight: 10,
   },
   inputText: {
     fontSize: 24,
-    fontFamily: 'dubai-regular',
-    justifyContent: 'center',
-    alignContent: 'center',
+    fontFamily: "dubai-regular",
+    justifyContent: "center",
+    alignContent: "center",
     marginRight: 10,
-    color: '#FFFFFF',
+    color: "#FFFFFF",
   },
   targetInput: {
     borderWidth: 1,
-    borderColor: '#777',
+    borderColor: "#777",
     padding: 6,
     margin: 2,
     width: 60,
-    backgroundColor: '#FFFFFF',
+    backgroundColor: "#FFFFFF",
     borderRadius: 10,
-    textAlign: 'center',
+    textAlign: "center",
     fontSize: 24,
-    fontFamily: 'ds-digit',
-    color: '#3D3FB8',
+    fontFamily: "ds-digit",
+    color: "#3D3FB8",
   },
   targetView: {
-    flexDirection: 'row',
+    flexDirection: "row",
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'flex-start',
+    justifyContent: "center",
+    alignItems: "flex-start",
   },
   backgroundImage: {
-    position: 'absolute',
+    position: "absolute",
     zIndex: -9999,
-    resizeMode: 'cover',
+    resizeMode: "cover",
   },
   tasbihView: {
     flex: 5,
-    justifyContent: 'flex-start',
-    alignItems: 'center',
+    justifyContent: "flex-start",
+    alignItems: "center",
   },
 });
 
